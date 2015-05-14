229cf8be4769cfcbb1beddcf2bc9c67a891643ec
Entercode <quotenter@gmail.com>
2015-05-14 14:49:52 +0900

    Add project files (netbeans, gradle and javafx)

---
+++ eduDSP/src/main/java/com/github/entercode/edudsp/eduDSP.java
@@ -0,0 +1,19 @@
+package com.github.entercode.edudsp;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class eduDSP extends Application {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+		
+		System.out.println("helloworld");
+    }
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+}
