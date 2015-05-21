d62bd5529708dcbb07b8e5a91e02171e115a5a9c
Entercode <quotenter@gmail.com>
2015-05-21 14:27:10 +0900

    Remove old linechart.xml

--- eduDSP/src/main/java/com/github/entercode/edudsp/eduDSP.java
+++ eduDSP/src/main/java/com/github/entercode/edudsp/eduDSP.java
@@ -1,6 +1,12 @@
 package com.github.entercode.edudsp;
 
+import java.io.File;
+import java.net.URL;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class eduDSP extends Application {
@@ -8,12 +14,20 @@ public class eduDSP extends Application {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-		
+		launch(args);
 		System.out.println("helloworld");
+		
     }
 
 	@Override
-	public void start(Stage primaryStage) throws Exception {
-		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	public void start(Stage stage) throws Exception {
+		
+		Parent root = FXMLLoader.load(getClass().getResource("/linechart.fxml"));
+    
+        Scene scene = new Scene(root, 300, 275);
+    
+        stage.setTitle("FXML Welcome");
+        stage.setScene(scene);
+        stage.show();
 	}
 }
