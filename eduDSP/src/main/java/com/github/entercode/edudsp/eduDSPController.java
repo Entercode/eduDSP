46ea790e34c1bf7acf48b76977c66f7e570ddedf
Entercode <quotenter@gmail.com>
2015-06-04 14:19:44 +0900

    Add deb canvas(for test bench)

--- eduDSP/src/main/java/com/github/entercode/edudsp/eduDSPController.java
+++ eduDSP/src/main/java/com/github/entercode/edudsp/eduDSPController.java
@@ -3,10 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.github.entercode.edudsp.controller;
+package com.github.entercode.edudsp;
 
+import com.github.entercode.edudsp.blockdiagram.BlockDiagramViewController;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.animation.AnimationTimer;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
@@ -22,24 +24,25 @@ public class eduDSPController implements Initializable {
 
 	@FXML
 	private Label fx_label_status;
-	public Canvas fx_canvas_blockdiagram;
+	
+	@FXML
+	private BlockDiagramViewController bdvc;
+	
+	@FXML
+	private Canvas fx_canvas_deb;
 	
 	public GraphicsContext gc;
+	
+	
 	
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// something
-		System.out.println("hoge");
-		fx_label_status.setText("loaded");
-		gc = fx_canvas_blockdiagram.getGraphicsContext2D();
 		
-		draw(gc);
-	}
-	
-	public void draw(GraphicsContext gc) {
-//		gc.drawImage(new Image(getClass().getResource("/yahagi.png").getPath()), 100, 100);
-		//gc.drawImage(new Image("/Users/entercode/Git/eduDSP/eduDSP/build/resources/main/yahagi.png"), 100, 100);
-		gc.drawImage(new Image("/image/yahagi.png"), 100, 100);
+		fx_label_status.setText("loaded");
+		
+		
 	}
 }
+
