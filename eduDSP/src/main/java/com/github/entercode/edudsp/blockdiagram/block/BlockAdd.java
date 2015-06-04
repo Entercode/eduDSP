3f8f63bef7eff5b811b50956c3e277bc73020db0
Entercode <quotenter@gmail.com>
2015-06-04 13:07:07 +0900

    Add BlockAdd(block type add)

---
+++ eduDSP/src/main/java/com/github/entercode/edudsp/blockdiagram/block/BlockAdd.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.entercode.edudsp.blockdiagram.block;
+
+/**
+ *
+ * @author entercode
+ */
+public class BlockAdd extends Block {
+
+	public BlockAdd(String name, int id, int width, int height, String relativePath) {
+		super(name, id, width, height, relativePath);
+	}
+}
